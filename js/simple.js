3ebb898e6b798f6ae87c6f4931c5a41201a612fd
StevenMad <steven-madhow@outlook.fr>
2017-01-02 01:29:54 +0100

    updated weather min max temperature and weather icon

--- js/simple.js
+++ js/simple.js
@@ -1,5 +1,7 @@
-var x=setInterval(getDate,1000);
+setInterval(getDate,1000);
 var t=setInterval(updateWeather,1000);
+getDailyWeather();
+setInterval(getDailyWeather,60000*60*24);
 function updateWeather()
 {
     $.ajax({
@@ -8,13 +10,60 @@ function updateWeather()
         success:function(obj,status,xhr){
             var chaine = obj['weather'][0]["description"];
             $('.current-temp').html(obj['main']['temp']+" ° C");
-            var min = obj['main']['temp_min']+" ° C";
-            var max = obj['main']['temp_max']+" ° C";
-            $('.min-max').html('MIN : '+min+' - MAX : '+max);
+            //var min = obj['main']['temp_min']+" ° C";
+            //var max = obj['main']['temp_max']+" ° C";
+            //$('.min-max').html('MIN : '+min+' - MAX : '+max);
             $('.weather-name').html(chaine);
             $('.weather-icon').html(addImage(chaine));
         }
     })
+}
+
+function getDailyWeather()
+{
+    $.ajax({
+        type:'GET',
+        url:'http://api.openweathermap.org/data/2.5/forecast?units=metric&q=Paris,fr&APPID=18f5f1e035cfd691cbb990ac6b06cb24',
+        success:function(obj,status,xhr){
+            console.log(obj);
+            getMinMax(obj['list']);
+        }
+    })
+}
+
+function getMinMax(list)
+{
+    var min=100;
+    var max=-100;
+    for(var i=0;i<list.length;i++)
+    {
+        if(isDailyDate(list[i]['dt']))
+        {
+            if(list[i]['main']['temp']<min)
+            {
+                min=list[i]['main']['temp'];
+            }
+            if(list[i]['main']['temp']>max)
+            {
+                max=list[i]['main']['temp'];
+            }
+        }
+    }
+    $('.min-max').html('MIN : '+min+'°C - MAX : '+max+'°C');
+}
+
+function isDailyDate(datetime)
+{
+    var date = new Date(datetime * 1000);
+    var day = getTime(date.getDate());
+    var month = getTime(date.getMonth()+1);
+
+    var currentDate = new Date();
+    var currentDay = getTime(currentDate.getDate());
+    var currentMonth = getTime(currentDate.getMonth()+1);
+    if(day==currentDay && month==currentMonth)
+        return true;
+    return false;
 }
 
 function getDate()
@@ -29,7 +78,6 @@ function getDate()
                     + getTime(currentdate.getHours()) + ":"  
                     + getTime(currentdate.getMinutes()) + ":" 
                     + getTime(currentdate.getSeconds());
-            console.log(datetime);
             $('.daytime').html(datetime);
         }
     })
@@ -51,13 +99,15 @@ function addImage(description)
         case "few clouds":{id=2;break;}
         case "scattered clouds":{id=3;break;}
         case "broken clouds":{id=4;break;}
+        case "light intensity drizzle":{id=5;break;}
         case "shower rain":{id=5;break;}
         case "rain":{id=6;break;}
         case "snow":{id=7;break;}
         case "light shower snow":{id=7;break;}
         case "thunderstorm":{id=8;break;}
+        case "fog":{id=9;break;}
         case "mist":{id=9;break;}
-        case "haze":{id=9;console.log("hello world");break;}
+        case "haze":{id=9;break;}
     }
     return '<img src="/img/'+id+'.png"></>';
 }
