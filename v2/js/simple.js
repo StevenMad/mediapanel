5ddc8cd7f0b7344d7bfa8e94562bb63fb5af9771
StevenMad <steven-madhow@outlook.fr>
2017-07-17 17:49:04 +0200

    update new version of the future mediapanel

---
+++ v2/js/simple.js
@@ -0,0 +1,130 @@
+setInterval(getDate,1000);
+var t=setInterval(updateWeather,1000);
+var i=0;
+getDailyWeather();
+setInterval(getDailyWeather,60000*60*10);
+function updateWeather()
+{
+    $.ajax({
+        type:'GET',
+        url: 'http://api.openweathermap.org/data/2.5/weather?units=metric&q=Paris,fr',
+        data:{
+            'APPID':'1387e96c083003433d600c8f14249f09'
+        },
+        success:function(obj,status,xhr){
+            var chaine = obj['weather'][0]["description"];
+            $('.current-temp').html(obj['main']['temp']+" ° C");
+            $('.weather-name').html(chaine);
+            $('.weather-icon').html(addImage(chaine));
+            i++;
+        }
+    })
+}
+
+function getDailyWeather()
+{
+    $.ajax({
+        type:'GET',
+        url:'http://api.openweathermap.org/data/2.5/forecast?units=metric&q=Paris,fr',
+        data:{
+            'APPID':'1387e96c083003433d600c8f14249f09'
+        },
+        success:function(obj,status,xhr){
+            console.log(obj);
+            getMinMax(obj['list']);
+        }
+    })
+}
+
+function getMinMax(list)
+{
+    var min=100;
+    var max=-100;
+    for(var i=0;i<list.length;i++)
+    {
+        if(isDailyDate(list[i]['dt']))
+        {
+            if(list[i]['main']['temp']<min)
+            {
+                min=list[i]['main']['temp'];
+            }
+            if(list[i]['main']['temp']>max)
+            {
+                max=list[i]['main']['temp'];
+            }
+        }
+    }
+    $('.min-max').html('MIN : '+min+'°C - MAX : '+max+'°C');
+}
+
+function isDailyDate(datetime)
+{
+    var date = new Date(datetime * 1000);
+    var day = getTime(date.getDate());
+    var month = getTime(date.getMonth()+1);
+
+    var currentDate = new Date();
+    var currentDay = getTime(currentDate.getDate());
+    var currentMonth = getTime(currentDate.getMonth()+1);
+    if(day==currentDay && month==currentMonth)
+        return true;
+    return false;
+}
+
+function getDate()
+{
+    $.ajax({
+        success:function(obj,status,xhr)
+        {
+            var currentdate = new Date(); 
+            var datetime = "Today : " + getTime(currentdate.getDate()) + "/"
+                    + getTime((currentdate.getMonth()+1))  + "/" 
+                    + getTime(currentdate.getFullYear()) + " <br/>"  
+                    + getTime(currentdate.getHours()) + ":"  
+                    + getTime(currentdate.getMinutes()) + ":" 
+                    + getTime(currentdate.getSeconds());
+            $('.daytime').html(datetime);
+        }
+    })
+}
+
+function getTime(time)
+{
+    if(time<10)
+        return '0'+time;
+    return time;
+}
+
+function addImage(description)
+{
+    var id=1;
+    switch(description)
+    {
+        case "clear sky":{id=1;break;}
+        case "few clouds":{id=2;break;}
+        case "scattered clouds":{id=3;break;}
+        case "broken clouds":{id=4;break;}
+        case "light intensity drizzle":{id=5;break;}
+        case "shower rain":{id=5;break;}
+        case "light rain":{id=5;break;}
+        case "rain":{id=6;break;}
+        case "light snow":{id=7;break;}
+        case "snow":{id=7;break;}
+        case "light shower snow":{id=7;break;}
+        case "thunderstorm":{id=8;break;}
+        case "fog":{id=9;break;}
+        case "mist":{id=9;break;}
+        case "haze":{id=9;break;}
+    }
+    return '<img src="/v2/img/'+id+'.png"></>';
+}
+
+function addInfo()
+{
+
+}
+
+function showTemp()
+{
+
+}
---
+++ v2/js/stif.js
@@ -0,0 +1,42 @@
+updateTrain();
+var time = setInterval(updateTrain,60000);
+
+function updateTrain()
+{
+    $.ajax({
+        type:'GET',
+        url:'https://api.sncf.com/v1/coverage/sncf/stop_areas/stop_area:OCE:SA:87545301/departures',
+        headers:{'Authorization':'5ebaef96-18ab-4de4-90a7-35883bf675ef'},
+        success:function(obj,type,xhr){
+            var liste = obj['departures'];
+            var horaires='';
+            for(var i=0;i<liste.length;i++)
+            {
+                var direction = liste[i]['display_informations']['direction'];
+                console.log(direction);
+                switch(direction)
+                {
+                    case "Montigny-Beauchamp (Beauchamp)":horaires+='<div class="horaire-item">'+addDate(liste[i])+"</div>";break;
+                    case "Pontoise (Pontoise)":horaires+='<div class="horaire-item">'+addDate(liste[i])+"</div>";break;
+                    case "Chaville-Velizy (Viroflay)":horaires+='<div class="horaire-item">'+addDate(liste[i])+"</div>";break;
+                    case "Paris-Auster.(RER C ) (Paris)":horaires+='<div class="horaire-item">'+addDate(liste[i])+"</div>";break;
+                }
+            }
+            $('.horaire-list').html(horaires);    
+            }
+    })
+}
+
+//84465dc0215e560016e05fb30acc8427b88cd55b41e24285c7fc4a89 
+//48.814222, 2.391250
+//ivry stop_area:OCE:SA:87545301
+//line:OCE:SN-87382218-87382481
+
+function addDate(objet)
+{
+    //20161227T181200
+    var horaire = objet['stop_date_time']['arrival_date_time'].substring(9);
+    var heure = horaire.substring(0,2);
+    var min = horaire.substring(2,4);
+    return heure+":"+min;
+}
